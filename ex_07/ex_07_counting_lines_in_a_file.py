c9f0e3c907a02d302245ea19eaee3410bc91d7d7
tugberkbastepe <tugberk.bastepe@gmail.com>
2023-05-21 19:07:32 +0300

    Added comments

--- ex_07/ex_07_counting_lines_in_a_file.py
+++ ex_07/ex_07_counting_lines_in_a_file.py
@@ -1,5 +1,12 @@
+"""This is our file handle fhand
+
+"""
 fhand = open('chat.txt')
 count = 0
+
+# Now we count the number of lines in fhand
 for line in fhand :
     count = count +1
+
+# We print the # of lines counted
 print('Line count: ', count)
